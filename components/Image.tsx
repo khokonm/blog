c4b0ea9aa956a96a6e3acc62f178fbece43c1df8
Khokon M <khokonads@gmail.com>
2025-02-22 17:44:19 +0600

    Fixed photo url path

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,8 +2,10 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
-)
+const Image = ({ src, ...rest }: ImageProps) => {
+  const isAbsoluteUrl = src.startsWith('http://') || src.startsWith('https://')
+  const imageSrc = isAbsoluteUrl ? src : `${basePath || ''}${src}`
+  return <NextImage src={imageSrc} {...rest} />
+}
 
 export default Image
