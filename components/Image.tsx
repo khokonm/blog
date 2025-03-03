3f3e48f69fff23dc0915bd9864468d61ef4bd825
Khokon M <khokonads@gmail.com>
2025-03-04 00:31:50 +0600

    Added ubuntu server setup post

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,7 +3,8 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, ...rest }: ImageProps) => {
-  const isAbsoluteUrl = typeof src === 'string' && (src.startsWith('http://') || src.startsWith('https://'))
+  const isAbsoluteUrl =
+    typeof src === 'string' && (src.startsWith('http://') || src.startsWith('https://'))
   const imageSrc = isAbsoluteUrl ? src : `${basePath || ''}${src}`
   return <NextImage src={imageSrc} {...rest} />
 }
